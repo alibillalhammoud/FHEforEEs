bb58a44f08c31fbc4cd996aa0c745ff96d9e9bf4
Ali Hammoud <70279295+alibillalhammoud@users.noreply.github.com>
2025-11-08 12:54:28 -0500

    misc

--- BFV_math.py
+++ BFV_math.py
@@ -1,9 +1,19 @@
 import numpy as np
+from BFV_config import BFVSchemeConfiguration
 
-# naive multiplication
-n = len(a)
-c = np.convolve(a, b)
-print(c)
-c_mod = c[:n].copy()
-for k in range(n, len(c)):
-    c_mod[k-n] -= c[k]
+
+def naive_polynomial_mult_nomod(config: BFVSchemeConfiguration, a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """Compute a*b mod x^n+1 naively (without NTT)"""
+    config.validateAB(a,b)
+    n = len(a)
+    c = np.convolve(a, b)
+    # compute c mod x^n+1
+    c_mod = c[:n].copy()
+    for k in range(n, len(c)):
+        c_mod[k-n] -= c[k]
+    # if you want mod uncomment this line
+    #c_mod %= config.q
+    return c_mod
+
+
+polynomial_mult_nomod = naive_polynomial_mult_nomod
--- BFV_model.py
+++ BFV_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from BFV_config import BFVSchemeConfiguration
+from BFV_math import polynomial_mult_nomod
 
 class BFVSchemeClient:
     def __init__(self, config: BFVSchemeConfiguration):
@@ -7,37 +8,27 @@ class BFVSchemeClient:
         :param config: the BFV scheme configuration containing all required parameters and settings
         """
         self.config = config
-        # Secret key: k polynomials of degree n-1, coefficients in {-1,0,1} or {0,1}, shape (k, n)
+        # Secret key: polynomial degree n-1, n coefficients in {-1,0,1} or {0,1}
         if config.ternary:
-            self._S = np.random.choice([-1, 0, 1], size=(k, n))
+            self._S = np.random.choice([-1, 0, 1], size=n)
         else:
-            self._S = np.random.choice([0, 1], size=(k, n))
+            self._S = np.random.choice([0, 1], size=n)
 
-#    def polynomial_mul(self, a, b):
-#        """
-#        Polynomial multiplication modulo x^n and coefficients modulo q.
-#        """
-#        result = np.convolve(a, b)[:self.config.n]
-#        if len(result) < self.config.n:
-#            result = np.pad(result, (0, self.config.n-len(result)))
-#        return result % self.config.q
-    def polynomial_mul(self, a, b):
-        return a
-        raise NotImplementedError("TODO: return the element wise product between point representations of both polynomials")
-        
+    def polynomial_mul_modq(self, A, B):
+        return polynomial_mult_nomod(self.config,A,B)
 
     def encrypt(self, M: np.ndarray):
         """
         Encrypts message polynomial M (coeffs mod t, degree n-1).
         Returns tuple (A, B) where:
-            A: shape (k, n), each row is a public polynomial mod q.
-            B: shape (n,), the result poly mod q.
+            A: size n, each row is a public polynomial mod q.
+            B: size n, the result poly mod q.
         """
         # Message encoding
-        M = np.array(M) % self.config.t                      # shape (n, )
-        DeltaM = (M * self.config.Delta) % self.config.q            # shape (n, )
-        # random A (public key),k polynomials with n coefficients mod q, shape (k, n)
-        A = np.random.randint(0, self.config.q, size=(self.config.k, self.config.n))
+        M = np.array(M) % self.config.t                      # size n
+        DeltaM = (M * self.config.Delta) % self.config.q            # size n
+        # random A (public key), n coefficients mod q, size n
+        A = np.random.randint(0, self.config.q, size=self.config.n)
         # small noise E (centered discrete gaussian), mod q
         E = np.round(np.random.normal(0, 1, size=self.config.n)).astype(int) % self.config.q
         # B = -A*S + DeltaM + E, all mod q, S is persistent secret key
@@ -55,12 +46,12 @@ class BFVSchemeClient:
         return message
 
 
-
 class BFVSchemeServer:
     def __init__(self, config: BFVSchemeConfiguration):
         self.config = config
 
     def encode(self, P):
+        # this is not really implemented yet (hopefully we dont need it)
         M = np.array(P) % self.config.t
         return M
     
